a93859da19ef1093ebadb7d99556532c6efef0c5
marstrong <marshallstrong123@gmail.com>
2021-01-03 12:46:53 -0500

    tell Rails to use the regular scaffolding process (neccessary due to ActiveAdmin using 'inherited_resources')

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module Yenius
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.app_generators.scaffold_controller = :scaffold_controller
 
     # Middleware for ActiveAdmin
     config.middleware.use Rack::MethodOverride
