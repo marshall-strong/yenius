af05a7cce557cf2f2508428d2db4970496bd4081
marstrong <marshallstrong123@gmail.com>
2021-01-06 14:24:46 -0500

    create CommentsContainer component to handle loading

---
+++ client/src/features/comments/CommentsContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import CommentsList from "./CommentsList";
+
+const CommentsContainer = ({ commentableId, commentableType }) => {};
+
+export default CommentsContainer;
