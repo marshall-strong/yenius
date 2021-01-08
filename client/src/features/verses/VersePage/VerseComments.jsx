220b0e40eaa23d7f9a8aea95a2f49ec5887f270e
marstrong <marshallstrong123@gmail.com>
2021-01-08 15:49:59 -0500

    add VersePage component (needs refactoring): The VersePage component works, but is not ideal. VersePage currently replaces the entire SongPage -- we probably want it to just add to the SongPage, and to hide TrackInfo instead of removing it altogether. VersePage does not error, but it fetches data when it may not need to, and it sends requests more than once.

---
+++ client/src/features/verses/VersePage/VerseComments.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectVerseById } from "../versesSlice";
+import { addVerseComment } from "../../comments/commentsAsyncThunks";
+import CommentsContainer from "../../comments/CommentsContainer";
+
+const VerseComments = ({ verseId }) => {
+  const verse = useSelector((state) => selectVerseById(state, verseId));
+
+  if (!verse || !verse.comments) {
+    return null;
+  } else {
+    return (
+      <CommentsContainer
+        addComment={addVerseComment}
+        commentableId={verseId}
+        commentableType={"Verse"}
+      />
+    );
+  }
+};
+
+export default VerseComments;
