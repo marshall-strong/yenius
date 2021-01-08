ec4116b06247db230375e488feb97c655d6a0bfb
marstrong <marshallstrong123@gmail.com>
2021-01-08 14:43:35 -0500

    add a new asyncThunk 'verses/fetchVersePage' for fetching all the data needed for VersePage

--- client/src/features/verses/versesAsyncThunks.js
+++ client/src/features/verses/versesAsyncThunks.js
@@ -1,2 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+
+export const fetchVersePage = createAsyncThunk(
+  "verses/fetchVersePage",
+  async (verseId) => {
+    const response = await axios.get(`/api/v1/verses/${verseId}`);
+    return response.data;
+  }
+);
