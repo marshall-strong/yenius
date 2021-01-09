0cdae3f1839d87bce1ec2c535da175072968d731
marstrong <marshallstrong123@gmail.com>
2021-01-09 17:19:06 -0500

    fix intersperse import in ArtistsLinks

--- client/src/features/artists/ArtistsLinks.js
+++ client/src/features/artists/ArtistsLinks.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import intersperse from "../../lib";
+import { intersperse } from "../../lib";
 import { selectArtistById } from "./artistsSlice";
 
 export const ArtistLink = ({ artistId }) => {
