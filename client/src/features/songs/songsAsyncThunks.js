fba061ce872d3aae1c482ae4a1ac113f708fb1c9
marstrong <marshallstrong123@gmail.com>
2021-01-10 17:31:43 -0500

    add songsAsyncThunks exports for the new SongPage backend routes

--- client/src/features/songs/songsAsyncThunks.js
+++ client/src/features/songs/songsAsyncThunks.js
@@ -24,3 +24,75 @@ export const fetchSongsIndex = createAsyncThunk(
     return response.data;
   }
 );
+
+export const fetchSongAlbum = createAsyncThunk(
+  "songs/fetchSongAlbum",
+  async (songId) => {
+    const response = await axios.get(`/api/v1/songs/${songId}/song_album`);
+    return response.data;
+  }
+);
+
+export const fetchSongAnnotations = createAsyncThunk(
+  "songs/fetchSongAnnotations",
+  async (songId) => {
+    const response = await axios.get(
+      `/api/v1/songs/${songId}/song_annotations`
+    );
+    return response.data;
+  }
+);
+
+export const fetchSongArtistCredits = createAsyncThunk(
+  "songs/fetchSongArtistCredits",
+  async (songId) => {
+    const response = await axios.get(
+      `/api/v1/songs/${songId}/song_artist_credits`
+    );
+    return response.data;
+  }
+);
+
+export const fetchSongBanner = createAsyncThunk(
+  "songs/fetchSongBanner",
+  async (songId) => {
+    const response = await axios.get(`/api/v1/songs/${songId}/song_banner`);
+    return response.data;
+  }
+);
+
+export const fetchSongComments = createAsyncThunk(
+  "songs/fetchSongComments",
+  async (songId) => {
+    const response = await axios.get(`/api/v1/songs/${songId}/song_comments`);
+    return response.data;
+  }
+);
+
+export const fetchSongDescription = createAsyncThunk(
+  "songs/fetchSongDescription",
+  async (songId) => {
+    const response = await axios.get(
+      `/api/v1/songs/${songId}/song_description`
+    );
+    return response.data;
+  }
+);
+
+export const fetchSongLyrics = createAsyncThunk(
+  "songs/fetchSongLyrics",
+  async (songId) => {
+    const response = await axios.get(`/api/v1/songs/${songId}/song_lyrics`);
+    return response.data;
+  }
+);
+
+export const fetchSongSampleCredits = createAsyncThunk(
+  "songs/fetchSongSampleCredits",
+  async (songId) => {
+    const response = await axios.get(
+      `/api/v1/songs/${songId}/song_sample_credits`
+    );
+    return response.data;
+  }
+);
