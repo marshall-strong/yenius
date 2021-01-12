5325034f0659971adf826bd09a5f85c3fb4cf313
marstrong <marshallstrong123@gmail.com>
2021-01-12 11:54:28 -0500

    SongPageRefactor: Loading: ColumnPrimary renders Lyrics and SongComments if all requests are fulfilled.

--- client/src/features/songs/SongPage/ColumnPrimary.jsx
+++ client/src/features/songs/SongPage/ColumnPrimary.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Lyrics from "./Lyrics";
 import SongComments from "./SongComments";
 
 const ColumnPrimary = ({ songId }) => {
+  const status = useSelector((state) => state.songs.status);
+  const { fetchSongComments, fetchSongLyrics } = status;
+  const isFulfilled = (request) => request === "fulfilled";
+
+  const lyricsRequests = [fetchSongLyrics];
+  const lyrics = lyricsRequests.every(isFulfilled) ? (
+    <Lyrics songId={songId} />
+  ) : (
+    <div className="loader" />
+  );
+
+  const songCommentsRequests = [fetchSongComments];
+  const songComments = songCommentsRequests.every(isFulfilled) ? (
+    <SongComments songId={songId} />
+  ) : (
+    <div className="loader" />
+  );
   return (
     <div className="column_layout-column_span column_layout-column_span--primary">
-      <Lyrics songId={songId} />
-      <SongComments songId={songId} />
+      {lyrics}
+      {songComments}
     </div>
   );
 };
