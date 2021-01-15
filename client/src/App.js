314191977bc30295277c50269e0ecb4db1d62e1c
marstrong <marshallstrong123@gmail.com>
2021-01-15 16:20:19 -0500

    update client /songs routes

--- client/src/App.js
+++ client/src/App.js
@@ -42,9 +42,9 @@ import ArtistPage from "./features/artists/ArtistPage";
 import AlbumsList from "./features/albums/AlbumsList";
 import AlbumPage from "./features/albums/AlbumPage";
 
-import SongsIndexCharIndex from "./features/songs/SongsIndex";
-import SongsIndexCharSongs from "./features/songs/SongsList";
-import SongPage from "./features/songs/SongShow";
+import SongsIndex from "./features/songs/SongsIndex";
+import SongsList from "./features/songs/SongsList";
+import SongShow from "./features/songs/SongShow";
 
 import UsersList from "./features/users/UsersList";
 import UserPage from "./features/users/UserPage";
@@ -52,59 +52,48 @@ import SignupForm from "./features/session/SignupForm";
 import LoginForm from "./features/session/LoginForm";
 import PageFooter from "./app/PageFooter";
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <main className="App">
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
+const App = () => (
+  <Router>
+    <Navbar />
+    <main className="App">
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
 
-          <Route exact path="/artists" component={ArtistsIndexCharIndex} />
-          <Route
-            exact
-            path="/artists/index/"
-            component={ArtistsIndexCharIndex}
-          />
-          <Route
-            exact
-            path="/artists/index/:char"
-            component={ArtistsIndexCharArtists}
-          />
-          <Route exact path="/artists/:artistId" component={ArtistPage} />
+        <Route exact path="/artists" component={ArtistsIndexCharIndex} />
+        <Route exact path="/artists/index/" component={ArtistsIndexCharIndex} />
+        <Route
+          exact
+          path="/artists/index/:char"
+          component={ArtistsIndexCharArtists}
+        />
+        <Route exact path="/artists/:artistId" component={ArtistPage} />
 
-          <Route exact path="/albums" component={AlbumsList} />
-          <Route exact path="/albums/:albumId" component={AlbumPage} />
+        <Route exact path="/albums" component={AlbumsList} />
+        <Route exact path="/albums/:albumId" component={AlbumPage} />
 
-          <Route exact path="/songs" component={SongsIndexCharIndex} />
-          <Route exact path="/songs/index" component={SongsIndexCharIndex} />
-          <Route
-            exact
-            path="/songs/index/:char"
-            component={SongsIndexCharSongs}
-          />
+        <Route exact path="/songs" component={SongsIndex} />
+        <Route exact path="/songs/index" component={SongsIndex} />
+        <Route exact path="/songs/index/:char" component={SongsList} />
+        <Route exact path="/songs/:songId" component={SongShow} />
+        <Route exact path="/songs/:songId/verses" component={SongShow} />
+        <Route
+          exact
+          path="/songs/:songId/verses/:verseId"
+          component={SongShow}
+        />
 
-          <Route exact path="/songs/:songId" component={SongPage} />
-          <Route exact path="/songs/:songId/verses" component={SongPage} />
-          <Route
-            exact
-            path="/songs/:songId/verses/:verseId"
-            component={SongPage}
-          />
+        <Route exact path="/users" component={UsersList} />
+        <Route exact path="/users/:userId" component={UserPage} />
 
-          <Route exact path="/users" component={UsersList} />
-          <Route exact path="/users/:userId" component={UserPage} />
+        <AuthorizedRoute exact path="/signup" component={SignupForm} />
+        <AuthorizedRoute exact path="/login" component={LoginForm} />
 
-          <AuthorizedRoute exact path="/signup" component={SignupForm} />
-          <AuthorizedRoute exact path="/login" component={LoginForm} />
-
-          <Route component={NotFound} />
-          <Redirect to="/" />
-        </Switch>
-      </main>
-      <PageFooter />
-    </Router>
-  );
-}
+        <Route component={NotFound} />
+        <Redirect to="/" />
+      </Switch>
+    </main>
+    <PageFooter />
+  </Router>
+);
 
 export default App;
