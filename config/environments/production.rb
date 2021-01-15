4b4508bf709f4b495f75e9d1ac07d3f5748af950
marstrong <marshallstrong123@gmail.com>
2021-01-15 13:55:18 -0500

    fix typo: config.assets.compile should be FALSE in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,5 +103,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.assets.compile = true
+  config.assets.compile = false
 end
