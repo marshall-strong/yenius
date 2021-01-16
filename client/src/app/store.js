39fd6547a4de4fc300600436abcd28d6e1ad9f06
marstrong <marshallstrong123@gmail.com>
2021-01-15 20:54:16 -0500

    remove the 'artistCredits' and 'artistCreditTypes' slices -- they are never used. artistCredits is currently passed as a key in the artists, albums, and songs slices of state. It does not need its own slice of state.

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -7,8 +7,6 @@ import albumsReducer from "../features/albums/albumsSlice";
 import songsReducer from "../features/songs/songsSlice";
 import versesReducer from "../features/verses/versesSlice";
 import commentsReducer from "../features/comments/commentsSlice";
-import artistCreditsReducer from "../features/artistCredits/artistCreditsSlice";
-import artistCreditTypesReducer from "../features/artistCreditTypes/artistCreditTypesSlice";
 import sampleCreditsReducer from "../features/sampleCredits/sampleCreditsSlice";
 import sampleCreditTypesReducer from "../features/sampleCreditTypes/sampleCreditTypesSlice";
 import sessionReducer from "../features/session/sessionSlice";
@@ -23,8 +21,6 @@ export default configureStore({
     songs: songsReducer,
     verses: versesReducer,
     comments: commentsReducer,
-    artistCredits: artistCreditsReducer,
-    artistCreditTypes: artistCreditTypesReducer,
     sampleCredits: sampleCreditsReducer,
     sampleCreditTypes: sampleCreditTypesReducer,
     session: sessionReducer,
