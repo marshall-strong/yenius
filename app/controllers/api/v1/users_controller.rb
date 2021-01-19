8bc33dab7eb12240d7d6314f610d39e75d42b2dd
marstrong <marshallstrong123@gmail.com>
2021-01-19 17:05:23 -0500

    fix broken signup -- UsersController#create was not rendering the correct view. Was rendering 'views/api/v1/users/show.json.jbuilder'. Changed to 'views/api/v1/session/current_user.json.jbuilder'. CurrentUser has been moved to the sessions slice.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,35 +4,35 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login!(@user)
-      render :show
+      render 'api/v1/session/current_user';
     else
       render json: @user.errors.full_messages, status: 401
     end
   end
-  
+
   # @route PATCH /api/v1/users/:id (api_v1_user)
   # @route PUT /api/v1/users/:id (api_v1_user)
   def update
     @user = selected_user
     if @user && @user.update_attributes(user_params)
-      render :show
+      render 'api/v1/session/current_user';
     elsif !@user
       render json: ['Could not locate user'], status: 400
     else
       render json: @user.errors.full_messages, status: 401
     end
   end
-  
+
   # @route GET /api/v1/users/:id (api_v1_user)
   def show
     @user = selected_user
   end
-  
+
   # @route GET /api/v1/users (api_v1_users)
   def index
     @users = User.all
   end
-  
+
   # @route DELETE /api/v1/users/:id (api_v1_user)
   def destroy
     @user = selected_user
@@ -43,13 +43,13 @@ class Api::V1::UsersController < ApplicationController
       render ['Could not find user']
     end
   end
-  
+
   private
-  
+
   def selected_user
     User.find(params[:id])
   end
-  
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
