2a34952903018e79e801fb4596828901b38d9bc0
marstrong <marshallstrong123@gmail.com>
2021-01-21 13:26:10 -0500

    update routes -- split up 'resource :users' into its component routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,33 +4,37 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      get  '/albums',                        to: 'albums#index'
-      get  '/albums/:album_id',              to: 'albums#show'
-      get  '/albums/:album_id/comments',     to: 'comments#album_comments'
-      post '/albums/:album_id/comments',     to: 'comments#create_album_comment'
+      get    '/albums',                        to: 'albums#index'
+      get    '/albums/:album_id',              to: 'albums#show'
+      get    '/albums/:album_id/comments',     to: 'comments#album_comments'
+      post   '/albums/:album_id/comments',     to: 'comments#create_album_comment'
 
-      get  '/artists/index/:char',           to: 'artists#index'
-      get  '/artists/:artist_id',            to: 'artists#show'
-      get  '/artists/:artist_id/comments',   to: 'comments#artist_comments'
-      post '/artists/:artist_id/comments',   to: 'comments#create_artist_comment'
+      get    '/artists/index/:char',           to: 'artists#index'
+      get    '/artists/:artist_id',            to: 'artists#show'
+      get    '/artists/:artist_id/comments',   to: 'comments#artist_comments'
+      post   '/artists/:artist_id/comments',   to: 'comments#create_artist_comment'
 
-      get  '/songs/index/:char',             to: 'songs#index'
-      get  '/songs/:song_id',                to: 'songs#show'
-      get  '/songs/:song_id/album',          to: 'songs#album'
-      get  '/songs/:song_id/artist_credits', to: 'songs#artist_credits'
-      get  '/songs/:song_id/banner',         to: 'songs#banner'
-      get  '/songs/:song_id/description',    to: 'songs#description'
-      get  '/songs/:song_id/lyrics',         to: 'songs#lyrics'
-      get  '/songs/:song_id/sample_credits', to: 'songs#sample_credits'
-      get  '/songs/:song_id/comments',       to: 'comments#song_comments'
-      post '/songs/:song_id/comments',       to: 'comments#create_song_comment'
+      get    '/songs/index/:char',             to: 'songs#index'
+      get    '/songs/:song_id',                to: 'songs#show'
+      get    '/songs/:song_id/album',          to: 'songs#album'
+      get    '/songs/:song_id/artist_credits', to: 'songs#artist_credits'
+      get    '/songs/:song_id/banner',         to: 'songs#banner'
+      get    '/songs/:song_id/description',    to: 'songs#description'
+      get    '/songs/:song_id/lyrics',         to: 'songs#lyrics'
+      get    '/songs/:song_id/sample_credits', to: 'songs#sample_credits'
+      get    '/songs/:song_id/comments',       to: 'comments#song_comments'
+      post   '/songs/:song_id/comments',       to: 'comments#create_song_comment'
 
-      get  '/verses/:verse_id',              to: 'verses#show'
-      get  '/verses/:verse_id/comments',     to: 'comments#verse_comments'
-      post '/verses/:verse_id/comments',     to: 'comments#create_verse_comment'
+      get    '/verses/:verse_id',              to: 'verses#show'
+      get    '/verses/:verse_id/comments',     to: 'comments#verse_comments'
+      post   '/verses/:verse_id/comments',     to: 'comments#create_verse_comment'
 
-      resources :users
-      get  '/users/top_scholars',            to: 'users#top_scholars'
+      get    '/users/top_scholars',            to: 'users#top_scholars'
+      get    '/users/:user_id',                to: 'users#show'
+      post   '/users',                         to: 'users#create'
+      patch  '/users/:user_id',                to: 'users#update'
+      put    '/users/:user_id',                to: 'users#update'
+      delete '/users/:user_id',                to: 'users#destroy'
 
       resource :session, only: [:create, :destroy]
     end
