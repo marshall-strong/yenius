ff443309a684a5946c7363afadb186baabbfb230
marstrong <marshallstrong123@gmail.com>
2021-01-21 16:08:10 -0500

    add a color palette to use for users

---
+++ lib/ms_palette.rb
@@ -0,0 +1,36 @@
+# https://colorswall.com/palette/73/
+# Color		  HEX			  RGB
+# msYellow	#fff100		rgb(255, 241, 0)
+# msOrange	#ff8c00		rgb(255, 140, 0)
+# msRed		  #e81123		rgb(232, 17, 35)
+# msMagenta	#ec008c		rgb(236, 0, 140)
+# msPurple	#68217a		rgb(104, 33, 122)
+# msBlue		#00188f		rgb(0, 24, 143)
+# msCyan		#00bcf2		rgb(0, 188, 242)
+# msTeal		#00b294		rgb(0, 178, 148)
+# msGreen		#009e49		rgb(0, 158, 73)
+# msLime		#bad80a		rgb(186, 216, 10)
+
+MS_YELLOW = "#fff100"
+MS_ORANGE = "#ff8c00"
+MS_RED = "#e81123"
+MS_MAGENTA = "#ec008c"
+MS_PURPLE = "#68217a"
+MS_BLUE = "#00188f"
+MS_CYAN = "#00bcf2"
+MS_TEAL = "#00b294"
+MS_GREEN	= "#009e49"
+MS_LIME = "#bad80a"
+
+MS_PALETTE = [
+  MS_YELLOW,
+  MS_ORANGE,
+  MS_RED,
+  MS_MAGENTA,
+  MS_PURPLE,
+  MS_BLUE,
+  MS_CYAN,
+  MS_TEAL,
+  MS_GREEN,
+  MS_LIME
+]
