05a7b5f2c5e19f7b4c9c4070fb0b34c307ab0d30
marstrong <marshallstrong123@gmail.com>
2021-01-22 13:17:39 -0500

    add song.top_song_number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_193933) do
+ActiveRecord::Schema.define(version: 2021_01_22_181052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,6 +147,7 @@ ActiveRecord::Schema.define(version: 2021_01_21_193933) do
     t.integer "album_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "top_song_number"
     t.index ["album_id"], name: "index_songs_on_album_id"
     t.index ["name"], name: "index_songs_on_name"
   end
