687e6eca756bede9e887fc47365d4fa4c6ac0192
marstrong <marshallstrong123@gmail.com>
2021-01-24 15:46:17 -0500

    combine TopChartsTitle.jsx into TopCharts.jsx

--- client/src/app/TopCharts.jsx
+++ client/src/app/TopCharts.jsx
@@ -1,87 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
+import Dropdown from "./TopChartsDropdown";
 import TopAlbums from "../features/albums/TopAlbums";
 import TopArtists from "../features/artists/TopArtists";
-import TopChartsTitle from "./TopChartsTitle";
 import TopSongs from "../features/songs/TopSongs";
 
 import "../assets/stylesheets/TopCharts.scss";
 
-const Dropdown = ({ setChart }) => {
-  const [showMenu, setShowMenu] = useState(false);
-
-  const openMenu = (e) => {
-    e.preventDefault();
-    setShowMenu(true);
-  };
-
-  useEffect(() => {
-    if (showMenu) {
-      document.addEventListener("click", closeMenu);
-    }
-  }, [showMenu]);
-
-  const closeMenu = (e) => {
-    e.preventDefault();
-    setShowMenu(false);
-    document.removeEventListener("click", closeMenu);
-  };
-
-  const handleAlbums = (e) => {
-    e.preventDefault();
-    setChart("albums");
-    closeMenu(e);
-  };
-
-  const handleArtists = (e) => {
-    e.preventDefault();
-    setChart("artists");
-    closeMenu(e);
-  };
-
-  const handleSongs = (e) => {
-    e.preventDefault();
-    setChart("songs");
-    closeMenu(e);
-  };
-
-  const menu = showMenu ? (
-    <div className="menu">
-      <button onClick={handleAlbums}>{"albums"}</button>
-      <button onClick={handleArtists}>{"artists"}</button>
-      <button onClick={handleSongs}>{"songs"}</button>
+const TopChartsTitle = () => (
+  <div className="PageGriddesktop-a6v82w-0 SectionTitle__Container-sc-10idewm-0 SectionTitle__Container">
+    <h2 className="PageGridFull-idpot7-0 SectionTitle__Title-sc-10idewm-1 SectionTitle__Title">
+      Charts
+    </h2>
+    <div className="PageGridCenter-q0ues6-0 SectionTitle__Subtitle-sc-10idewm-2 SectionTitle__Subtitle">
+      <h3
+        color="accent.main"
+        fontWeight="normal"
+        className="TextLabel__TopChartsTitle"
+      >
+        Trending on Yenius
+      </h3>
     </div>
-  ) : (
-    <div className="menu"></div>
-  );
+  </div>
+);
 
-  return (
-    <div className="menu">
-      <button onClick={openMenu}>{"change chart"}</button>
-      {menu}
-    </div>
-  );
+const TopChartsItems = ({ chartType }) => {
+  let chartsItems;
+  if (chartType === "albums") {
+    chartsItems = <TopAlbums />;
+  } else if (chartType === "artists") {
+    chartsItems = <TopArtists />;
+  } else if (chartType === "songs") {
+    chartsItems = <TopSongs />;
+  } else {
+    chartsItems = <div>Something unexpected happened in TopCharts...</div>;
+  }
+  return chartsItems;
 };
 
 const TopCharts = () => {
-  const [chart, setChart] = useState("songs");
-
-  let content;
-  if (chart === "albums") {
-    content = <TopAlbums />;
-  } else if (chart === "artists") {
-    content = <TopArtists />;
-  } else if (chart === "songs") {
-    content = <TopSongs />;
-  } else {
-    content = <div>Something unexpected happened in TopCharts...</div>;
-  }
-
+  const [chartType, setChartType] = useState("songs");
   return (
     <div className="TopCharts">
       <TopChartsTitle />
-      <Dropdown setChart={setChart} />
-      {content}
+      <Dropdown chartType={chartType} setChartType={setChartType} />
+      <TopChartsItems chartType={chartType} />
     </div>
   );
 };
