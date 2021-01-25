acebac4db7c8e4d39efae890ba5ee3f9d8bd11ef
marstrong <marshallstrong123@gmail.com>
2021-01-25 14:01:59 -0500

    add user.my_color in the original migration

--- db/migrate/20200304014539_create_users.rb
+++ db/migrate/20200304014539_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :email, null: false
       t.string :password_digest, null: false
       t.string :session_token, null: false
+      t.string :my_color, null: false
 
       t.timestamps
     end
--- db/migrate/20210121193933_add_my_color_to_users.rb
+++ db/migrate/20210121193933_add_my_color_to_users.rb
@@ -1,5 +1,5 @@
-class AddMyColorToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :my_color, :string
-  end
-end
+# class AddMyColorToUsers < ActiveRecord::Migration[6.0]
+#   def change
+#     add_column :users, :my_color, :string
+#   end
+# end
