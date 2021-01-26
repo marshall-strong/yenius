2fa0b04395d293019ecc065a2069c147bc4bef90
marstrong <marshallstrong123@gmail.com>
2021-01-26 15:37:16 -0500

    get rid of MockComponents

--- client/src/features/albums/ColumnSecondary.jsx
+++ client/src/features/albums/ColumnSecondary.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
 import Description from "./Description";
-import MockComponent from "./MockComponent";
 
 const ColumnSecondary = ({ albumId }) => {
   return (
     <div className="column_layout-column_span column_layout-column_span--secondary u-top_margin column_layout-flex_column">
       <div className="column_layout-column_span-initial_content">
         <Description albumId={albumId} />
-        <MockComponent />
       </div>
       <div className="column_layout-flex_column-fill_column"></div>
     </div>
