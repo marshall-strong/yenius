4ccfbb69fc0fcaca74b73a89407b4290d8cb2527
marstrong <marshallstrong123@gmail.com>
2021-01-26 13:25:20 -0500

    update Artist Description: if description is over 655 characters (*cough* KANYE *cough*), display only part of the description and show a button for displaying the entire description

--- client/src/features/artists/Description.jsx
+++ client/src/features/artists/Description.jsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 import { selectArtistById } from "./artistsSlice";
 
 const Description = ({ artistId }) => {
   const artist = useSelector((state) => selectArtistById(state, artistId));
-  const markup = artist ? { __html: artist.bio } : null;
-  return (
-    <div className="MockComponent">
-      <h1>Description</h1>
-      <span dangerouslySetInnerHTML={markup} />
-    </div>
-  );
+  if (!artist || !artist.bio) {
+    return null;
+  }
+
+  let bio;
+  const needButton = artist.bio.length > 700;
+
+  const [isExpanded, setExpanded] = useState(false);
+  const handleClick = (e) => {
+    e.preventDefault();
+    setExpanded(true);
+  };
+
+  if (needButton) {
+    const markupInitial = { __html: artist.bio.slice(0, 655) };
+    const markup = { __html: artist.bio };
+    bio = isExpanded ? (
+      <div className="bio">
+        <span dangerouslySetInnerHTML={markup} />
+      </div>
+    ) : (
+      <div className="bio">
+        <span dangerouslySetInnerHTML={markupInitial} />
+        <button onClick={handleClick}>show full description</button>
+      </div>
+    );
+  } else {
+    const markup = { __html: artist.bio };
+    bio = (
+      <div className="bio">
+        <span dangerouslySetInnerHTML={markup} />
+      </div>
+    );
+  }
+
+  return <div className="Description">{bio}</div>;
 };
 
 const Loader = ({ artistId }) => {
