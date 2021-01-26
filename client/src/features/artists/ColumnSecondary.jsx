10748a6b9177a27fd700ac890932b9df695a20d2
marstrong <marshallstrong123@gmail.com>
2021-01-26 12:48:21 -0500

    remove MockComponents from Artist Show page

--- client/src/features/artists/ColumnSecondary.jsx
+++ client/src/features/artists/ColumnSecondary.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
 import Description from "./Description";
-import MockComponent from "./MockComponent";
 
 const ColumnSecondary = ({ artistId }) => {
   return (
     <div className="column_layout-column_span column_layout-column_span--secondary u-top_margin column_layout-flex_column">
       <div className="column_layout-column_span-initial_content">
         <Description artistId={artistId} />
-        <MockComponent />
       </div>
       <div className="column_layout-flex_column-fill_column"></div>
     </div>
