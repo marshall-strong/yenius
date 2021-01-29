437ccfedc2696d5ddf168c4cbce7a46318e95ff5
marstrong <marshallstrong123@gmail.com>
2021-01-29 11:32:57 -0500

    add styling for AnnotationSidebar component

--- client/src/features/songs/AnnotationSidebar.jsx
+++ client/src/features/songs/AnnotationSidebar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "../../stylesheets/AnnotationSidebar.scss";
+
 const AnnotationSidebar = () => (
   <div className="AnnotationSidebar" referent="current_referent">
     <div
