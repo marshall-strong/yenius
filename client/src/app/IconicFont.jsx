aecc59b4ef666fee7b557c696537e614a6aa510d
marstrong <marshallstrong123@gmail.com>
2021-02-01 09:40:25 -0500

    add iconmonstr SVG -- menuHamburger

--- client/src/app/IconicFont.jsx
+++ client/src/app/IconicFont.jsx
@@ -205,6 +205,18 @@ export const menuDropdown = (
   </svg>
 );
 
+// https://iconmonstr.com/menu-1-svg/
+export const menuHamburger = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+  </svg>
+);
+
 // https://iconmonstr.com/paintbrush-1-svg/
 export const paintbrush = (
   <svg
