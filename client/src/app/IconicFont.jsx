77a24f5a80c58bf6f79f1dc54f6e604054d1dbf2
marstrong <marshallstrong123@gmail.com>
2021-02-01 09:47:42 -0500

    add iconmonstr SVG -- circleMinus, circlePlus

--- client/src/app/IconicFont.jsx
+++ client/src/app/IconicFont.jsx
@@ -37,6 +37,30 @@ export const circleInfo = (
   </svg>
 );
 
+// https://iconmonstr.com/minus-5-svg/
+export const circleMinus = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z" />
+  </svg>
+);
+
+// https://iconmonstr.com/plus-5-svg/
+export const circlePlus = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
+  </svg>
+);
+
 // https://iconmonstr.com/warning-1-svg/
 export const circleWarning = (
   <svg
