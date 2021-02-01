47108ccb38e39f7facddc6c31313754d8aee3a13
marstrong <marshallstrong123@gmail.com>
2021-02-01 09:41:13 -0500

    add iconmonstr SVG -- quoteLeft, quoteRight

--- client/src/app/IconicFont.jsx
+++ client/src/app/IconicFont.jsx
@@ -229,6 +229,30 @@ export const paintbrush = (
   </svg>
 );
 
+// https://iconmonstr.com/quote-1-svg/
+export const quoteLeft = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" />
+  </svg>
+);
+
+// https://iconmonstr.com/quote-3-svg/
+export const quoteRight = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M9.983 3v7.391c0 5.704-3.731 9.57-8.983 10.609l-.995-2.151c2.432-.917 3.995-3.638 3.995-5.849h-4v-10h9.983zm14.017 0v7.391c0 5.704-3.748 9.571-9 10.609l-.996-2.151c2.433-.917 3.996-3.638 3.996-5.849h-3.983v-10h9.983z" />
+  </svg>
+);
+
 // https://iconmonstr.com/media-control-55-svg/
 export const random = (
   <svg
