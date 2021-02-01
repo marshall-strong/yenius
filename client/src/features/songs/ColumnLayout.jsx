5eb375d41a8de72ec2c7af52a611efdd52442956
marshall-strong <marshallstrong123@gmail.com>
2021-02-01 17:58:13 -0500

    clean up comments

--- client/src/features/songs/ColumnLayout.jsx
+++ client/src/features/songs/ColumnLayout.jsx
@@ -6,29 +6,6 @@ import SongComments from "./SongComments";
 
 import "../.././stylesheets/ColumnLayout.scss";
 
-// const ColumnPrimary = ({ match, selectedVerseRef }) => {
-//   const songId = parseInt(match.params.songId);
-//   const verseId = parseInt(match.params.verseId);
-//   return (
-//     <div className="column_layout-column_span column_layout-column_span--primary">
-//       <Lyrics
-//         selectedVerseId={verseId}
-//         selectedVerseRef={selectedVerseRef}
-//         songId={songId}
-//       />
-//       <SongComments songId={songId} />
-//     </div>
-//   );
-// };
-
-// const ColumnSecondary = ({ match, selectedVerseRef}) => {
-//   return (
-//     <div className="column_layout-column_span column_layout-column_span--secondary">
-//       <ColumnSecondary match={match} selectedVerseRef={selectedVerseRef} />
-//     </div>
-//   );
-// };
-
 const ColumnLayout = ({ match }) => {
   const selectedVerseRef = useRef();
   const songId = parseInt(match.params.songId);
@@ -49,9 +26,7 @@ const ColumnLayout = ({ match }) => {
   return (
     <div className="song_body column_layout">
       {columnPrimary}
-      {/* <ColumnPrimary match={match} selectedVerseRef={selectedVerseRef} /> */}
       {columnSecondary}
-      {/* <ColumnSecondary selectedVerseRef={selectedVerseRef} match={match} /> */}
     </div>
   );
 };
