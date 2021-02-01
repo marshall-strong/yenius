ff7c137a56ae41bcc4655ecd8c4985f942c5d598
marstrong <marshallstrong123@gmail.com>
2021-02-01 08:50:15 -0500

    add iconmonstr SVG -- floppyDisk

--- client/src/app/IconicFont.jsx
+++ client/src/app/IconicFont.jsx
@@ -73,6 +73,18 @@ export const flag = (
   </svg>
 );
 
+// https://iconmonstr.com/save-14-svg/
+export const floppyDisk = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M14 3h2.997v5h-2.997v-5zm9 1v20h-22v-24h17.997l4.003 4zm-17 5h12v-7h-12v7zm14 4h-16v9h16v-9z" />
+  </svg>
+);
+
 // https://iconmonstr.com/gear-1-svg/
 export const gear = (
   <svg
