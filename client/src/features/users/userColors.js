66a15b8f1386703c57fac566968f905f1b03920b
marshall-strong <marshallstrong123@gmail.com>
2021-02-02 14:49:12 -0500

    move userColors to a separate file

---
+++ client/src/features/users/userColors.js
@@ -0,0 +1,76 @@
+// https://colorswall.com/palette/73/
+
+const yellow = {
+  name: "yellow",
+  funName: "Yeezy Yellow",
+  hexCode: "#fff100",
+};
+
+const orange = {
+  name: "orange",
+  funName: "Omari Orange",
+  hexCode: "#ff8c00",
+};
+
+const red = {
+  name: "red",
+  funName: "Runway Red",
+  hexCode: "#e81123",
+};
+
+const magenta = {
+  name: "magenta",
+  funName: "Manic Magenta",
+  hexCode: "#ec008c",
+};
+
+const purple = {
+  name: "purple",
+  funName: "Presidential Purple",
+  hexCode: "#68217a",
+};
+
+const blue = {
+  name: "blue",
+  funName: "Bipolar Blue",
+  hexCode: "#00188f",
+};
+
+const cyan = {
+  name: "cyan",
+  funName: "Chi-Town Cyan",
+  hexCode: "#00bcf2",
+};
+
+const teal = {
+  name: "teal",
+  funName: "Tabloid Teal",
+  hexCode: "#00b294",
+};
+
+const green = {
+  name: "green",
+  funName: "Genius Green",
+  hexCode: "#009e49",
+};
+
+const lime = {
+  name: "lime",
+  funName: "Let-You-Finish Lime",
+  hexCode: "#bad80a",
+};
+
+const userColors = [
+  yellow,
+  orange,
+  red,
+  magenta,
+  purple,
+  blue,
+  cyan,
+  teal,
+  green,
+  lime,
+];
+
+export default userColors;
