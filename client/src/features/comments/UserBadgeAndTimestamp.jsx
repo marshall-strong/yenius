2b5db3ae164753c6c641bd08a476e452595c4aa1
marshall-strong <marshallstrong123@gmail.com>
2021-02-02 01:10:38 -0500

    add UserBadgeAndTimestamp component

---
+++ client/src/features/comments/UserBadgeAndTimestamp.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+import { selectUserById } from "../users/usersSlice";
+
+import TimeAgo from "./TimeAgo";
+
+import "../../stylesheets/UserBadgeAndTimestamp.scss";
+
+const UserBadgeAndTimestamp = ({ userId, createdAt }) => {
+  const user = useSelector((state) => selectUserById(state, userId));
+  const username = user.username;
+  const userIQ = user.authoredCommentsCount;
+  const userColor = user.myColor;
+
+  return (
+    <div className="user_badge_and_timestamp">
+      <div className="user_badge_and_timestamp-badge">
+        <user-badge>
+          <Link
+            className="user_badge drop-target drop-element-attached-middle drop-target-attached-middle drop-element-attached-left drop-target-attached-right"
+            to={`/users/${userId}`}
+          >
+            <div className="user_avatar user_avatar--x_small clipped_background_image"></div>
+            <div className="user_badge-text">
+              <div className="user_badge-login_and_iq user_badge-login_and_iq--one_line">
+                <span className="user_badge-login">{username}</span>
+                <role-icon>
+                  <span className="user_badge-role_icon user_badge-role_icon--contributor">
+                    <svg
+                      src="role_icon_contributor_equilateral_triangle.svg"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 10 10"
+                    >
+                      <polygon points="5,1 10,9 0,9"></polygon>
+                    </svg>
+                  </span>
+                </role-icon>
+                <span className="user_badge-iq user_badge-iq--one_line">
+                  {userIQ}
+                </span>
+              </div>
+            </div>
+          </Link>
+        </user-badge>
+      </div>
+      <div className="user_badge_and_timestamp-created_at">
+        <TimeAgo timestamp={createdAt} />
+      </div>
+    </div>
+  );
+};
+
+export default UserBadgeAndTimestamp;
